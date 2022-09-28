37f3fed9a506e050b96f9f2353776013b707cfdc
Ali <aliaqel2021@gmail.com>
2022-09-28 18:58:05 +0300

    add message app

---
+++ Lab_3/Controllers/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Lab_3.Hubs;
+using Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Lab_3.Controllers
+{
+    [ApiController]
+    [Route("api/message")]
+    public class MessageController : Controller
+    {
+        protected readonly IHubContext<MessageHub> _messageHub;
+
+        public MessageController([NotNull]IHubContext<MessageHub> messageHub)
+        {
+            _messageHub = messageHub;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(MessagePost messagePost)
+        {
+            await _messageHub.Clients.All.SendAsync("sendToReact", "The message '" +
+                                                                   messagePost.Message + "' has been received");
+            return Ok();
+        }
+    }
+}
---
+++ Lab_3/Models/MessagePost.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab_3.Models
+{
+    public class MessagePost
+    {
+        public virtual string Message { get; set; }
+    }
+}
