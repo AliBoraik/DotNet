3d724999a803187b9947633b4b597c0e2f05ed3a
Olesya-Gerasimova <ogerasimova966@gmail.com>
2022-11-10 12:10:54 +0300

    Added mongodb

---
+++ ChatApp/Chat.Infrastructure/MongoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Chat.Domain.Dto;
+
+namespace Chat.Infrastructure;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+public class MongoDbContext
+{
+    readonly MongoDbCollection<Music> music;
+    readonly MongoDbCollection<Video> video;
+    readonly MongoDbCollection<Image> image;
+
+    public MongoDbContext(
+        IOptions<MongoDbSettings> mongoDbSettings)
+    {
+        var mongoClient = new MongoClient(
+            mongoDbSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            mongoDbSettings.Value.DatabaseName);
+
+        music = new MongoDbCollection<Music>(
+            mongoDatabase, mongoDbSettings.Value.MusicCollectionName);
+        
+        video = new MongoDbCollection<Video>(
+            mongoDatabase, mongoDbSettings.Value.VideoCollectionName);
+
+        image = new MongoDbCollection<Image>(
+            mongoDatabase, mongoDbSettings.Value.ImageCollectionName);
+
+    }
+}
