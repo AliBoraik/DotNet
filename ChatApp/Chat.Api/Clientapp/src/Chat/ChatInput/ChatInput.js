5250b9db82e3fdf5b7bc5c95c80a514d5034724d
Ali <aliaqel2021@gmail.com>
2022-11-24 02:15:55 +0300

    add select options and renderSelectedForm for metadata

--- ChatApp/Chat.Api/Clientapp/src/Chat/ChatInput/ChatInput.js
+++ ChatApp/Chat.Api/Clientapp/src/Chat/ChatInput/ChatInput.js
@@ -1,4 +1,7 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import Music from "../ChatWindow/MetaDataModels/Music";
+import Image from "../ChatWindow/MetaDataModels/Image";
+import Video from "../ChatWindow/MetaDataModels/Video";
 
 const getFileType = (file) => {
     const mime = file.current.files[0].type;
@@ -12,21 +15,40 @@ const ChatInput = ({sendMessage}) => {
     const username = useRef(null);
     const contents = useRef(null);
     const file = useRef(null);
+    const [fileType,setFileType] = useState();
+    const metaData = useRef([]);
     const [error, setError] = useState("");
-
+    
+    const handleChange = (event) => {
+       setFileType(event.target.value);
+    }
+    useEffect(() => {
+        file.current.focus();
+    }, []);
+    const renderSelectedForm = () =>{
+        switch(fileType) {
+            case 'image':
+                return <Image metaData={metaData}/>
+            case 'video':
+                return  <Video metaData={metaData}/>
+            case 'audio':
+                return  <Music metaData={metaData}/>
+            default:
+                return null;
+        }
+    }
     const onSubmit = (e) => {
         e.preventDefault();
-        
         if (username.current.value && contents.current.value) {
-            const data = {
-                username: username.current.value,
-                contents: contents.current.value,
-                file: file.current.files[0],
-                fileType: getFileType(file.current.files[0])
-            };
-            console.log(data);
-            sendMessage(data);
-        } else {
+             const data = {
+                 username: username.current.value,
+                 contents: contents.current.value,
+                 file: file.current.files[0],
+                 metadata: metaData.current  
+             };
+                sendMessage(data);
+            } 
+        else {
             if (!username.current.value) {
                 username.current.focus();
                 setError("Please provide a username");
@@ -63,7 +85,18 @@ const ChatInput = ({sendMessage}) => {
                 title="File"
                 ref={file}
             />
-            <br/><br/>
+            <br />
+            <div>
+                <label>Select MetaData...</label><br/>
+                <select value={fileType} onChange={handleChange}>
+                    <option value="Select metaData..." selected="selected"></option>
+                    <option value="image">Image</option>
+                    <option value="video">Video</option>
+                    <option value="audio">Audio</option>
+                </select>
+                {renderSelectedForm()}
+                <br/>
+            </div>
             <p style={{color: "red"}}>{error}</p>
             <button
                 type="submit"
