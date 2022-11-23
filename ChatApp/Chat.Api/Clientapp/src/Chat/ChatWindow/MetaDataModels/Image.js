cb1d7b4ce1f8bfabc98fc63ed530e85edbfafabe
Ali <aliaqel2021@gmail.com>
2022-11-24 02:14:58 +0300

    add image component

---
+++ ChatApp/Chat.Api/Clientapp/src/Chat/ChatWindow/MetaDataModels/Image.js
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+
+const Image  = (props) => {
+    const fields = ["Name","Type","Author","Resolution"]
+    return fields.map((field, index)=> {
+        return (
+            <div>
+                <label htmlFor={field}>{field} : </label>
+                <br/>
+                <input
+                    key={index}
+                    type="text"
+                    id={field}
+                    title={field}
+                    ref={(element) => props.metaData.current.push(element)}
+                />
+            </div>
+        )
+    })
+};
+
+
+export default Image;
