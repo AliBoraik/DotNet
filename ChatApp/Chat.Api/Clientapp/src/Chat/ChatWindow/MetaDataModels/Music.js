677c1ac62ab939e2e6ae0d31b0617998f03a48a8
Ali <aliaqel2021@gmail.com>
2022-11-24 02:15:09 +0300

    add music component

---
+++ ChatApp/Chat.Api/Clientapp/src/Chat/ChatWindow/MetaDataModels/Music.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+
+const Music  = (props) => {
+    const fields = ["Name","Artist","Album"]
+    return fields.map(field => {
+        return (
+            <div>
+                <label htmlFor={field}>{field} : </label>
+                <br/>
+                <input
+                    type="text"
+                    id={field}
+                    title={field}
+                    ref={(element) => props.metaData.current.push(element)}
+                />
+            </div>
+        )
+    })
+};
+
+export default Music;
